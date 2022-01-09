1fd7b24605474bf4f529ba3975cecb05a387dbb2
Endrey Márk <ungazik@gmail.com>
2022-01-09 16:04:02 +0100

    In this context, CL-zipper is a more precise name than CL-regex

--- CL/CL_zipper.hs
+++ CL/CL_zipper.hs
@@ -1,4 +1,4 @@
-module CL.CL_redex where
+module CL.CL_zipper where
 
 import CL.Base (Base)
 import CL.CL (reduction_rules)
@@ -7,9 +7,9 @@ import Data.Tree2_zipper (Tree2_zipper, move, rightward, upWhenDir, leftmost, Di
 import Data.Tuple.HT (mapFst)
 import Prelude hiding (Left)
 
-type CL_redex = Tree2_zipper Base
+type CL_zipper = Tree2_zipper Base
 
-reduction_step, left_path_reduction_step, local_reduction_step  :: CL_redex -> Maybe CL_redex
+reduction_step, left_path_reduction_step, local_reduction_step  :: CL_zipper -> Maybe CL_zipper
 reduction_step = tryAndAdjustTill left_path_reduction_step $ move rightward
 left_path_reduction_step = tryAndAdjustTill local_reduction_step (upWhenDir Left) . leftmost
 local_reduction_step (cl, cl_back) = do
--- Main.hs
+++ Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import CL.CL_redex
+import CL.CL_zipper
 
 main :: IO ()
 main = return ()
